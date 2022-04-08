646f51b755093d6797161b4f60cdb6db6c8af85e
MarcRamis <marc.ramis@enti.cat>
2022-04-08 13:28:21 +0200

    udp connection

--- GameClient/Peer.cpp
+++ GameClient/Peer.cpp
@@ -4,6 +4,23 @@
 
 int main()
 {
-	std::cout << "AA" << std::endl;
+	// Crear socket y hacer bind al puerto 55001
+	sf::UdpSocket socket;
+	socket.bind(55001);
+
+	while (true)
+	{
+		// Enviar mensaje al Puerto 55002 en la IP 192.168.1.50
+		std::string message = "Mi IP : " + sf::IpAddress::getLocalAddress().toString();
+		socket.send(message.c_str(), message.size() + 1, sf::IpAddress::getLocalAddress().toString(), 55002);
+		
+		// Recepción de respuesta
+		char buffer[1024];
+		std::size_t received = 0;
+		sf::IpAddress sender;
+		unsigned short port;
+		socket.receive(buffer, sizeof(buffer), received, sender, port);
+	}
+
 	return 0;
 }
--- GameServer/Server.cpp
+++ GameServer/Server.cpp
@@ -1,5 +1,26 @@
 
+#include <Selector.h>
+
 int main()
 {
+	// Crear Socket y hacer el bind al puerto 55002
+	sf::UdpSocket socket;
+	socket.bind(55002);
+
+	while (true)
+	{
+		// Recibir mensaje
+		char buffer[1024];
+		std::size_t received = 0;
+		sf::IpAddress sender;
+		unsigned short port;
+		socket.receive(buffer, sizeof(buffer), received, sender, port);
+		std::cout << sender.toString() << " dice: " << buffer << std::endl;
+		
+		// Envío de respuesta
+		std::string message = "Bienvenido " + sender.toString();
+		socket.send(message.c_str(), message.size() + 1, sender, port);
+	}
+
 	return 0;
 }
