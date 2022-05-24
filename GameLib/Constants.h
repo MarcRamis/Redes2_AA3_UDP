74eca9b6506b60a191793dcc6ff1c4a4455e081e
MarcRamis <marc.ramis@enti.cat>
2022-05-24 11:33:41 +0200

    just changed a constant

--- GameLib/Constants.h
+++ GameLib/Constants.h
@@ -6,4 +6,4 @@ const int BYTE_CAPACITY { 10240 };
 const float TIMESTAMP_DEFAULT{ 10.f };
 const int TRIES_DEFAULT { 2 };
 const float T_SEND{ 0.5f };
-const float T_INACTIVITY { 60.f };
+const float T_INACTIVITY { 30.f };
